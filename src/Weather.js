a865978aa793d6119d78e9698740ee9f02d4b316
Matilde Moretti <matilde.moretti95@gmail.com>
2023-11-07 17:59:13 +0100

    Added axios integration to show the weather

---
+++ src/Weather.js
@@ -0,0 +1,30 @@
+import React from "react";
+import axios from "axios";
+import { Oval } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${props.city} is ${Math.round(response.data.main.temp)}°C`
+    );
+  }
+
+  let apiKey = "197ef3a642b76eef90e131866f74a0a0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <Oval
+      height={80}
+      width={80}
+      color="#4fa94d"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+      ariaLabel="oval-loading"
+      secondaryColor="#4fa94d"
+      strokeWidth={2}
+      strokeWidthSecondary={2}
+    />
+  );
+}
